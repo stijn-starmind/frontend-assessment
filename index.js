eb225de9900c0f89b65db62cc59312968e6ccb3d
retoheusser <retoheusser@windowslive.com>
2018-03-21 15:40:44 +0100

    add libraries and describe tasks

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
-console.log("hello world")
+//import $ from "jquery";
+//import angular from "angular";
+//import _ from "lodash";
 
+/*
+TASK 1: https://sport.api.swisstxt.ch/v1/eventItems?phaseIds=3404-195 provides you with all 12 matches from
+this year's Champions League group stage from group A. Fetch this JSON endpoint and process the data such that you
+can display the final ranking table with each team's name and the number of points.
+A victory counts 3 points, a draw 1 point and a loss 0 points.
+
+See final table at https://www.srf.ch/sport/resultcenter/results#football/champions-league/GroupPhase-1-4/Group-2-0
+*/
+
+/*
+TASK 2: Add a button to the HTML which reverses the sort order of the table.
+*/
+
+/*
+TASK 3: Add a column to the table displaying the total number of points of the team if every game consists just of the first half.
+*/
