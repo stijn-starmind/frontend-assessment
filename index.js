49eb316b773cee6398a34625d4fe30e18d0e8143
retoheusser <retoheusser@windowslive.com>
2018-03-22 09:31:23 +0100

    angular solution third task

--- index.js
+++ index.js
@@ -9,12 +9,6 @@ season's group stage. Display a table with the sorted ranking according to the G
 and the goals scored column.
 */
 const app = angular.module("assessment", []);
-app.controller("tableController", ($http, $scope) => {
-	$http.get("https://sport.api.swisstxt.ch/v1/rankings/8481?lang=de").then(response => {
-		$scope.ranking = orderBy(response.data.rankingItems, "goalPlus", "desc");
-	});
-});
-
 
 /*
 TASK 2: Add a button to the HTML which reverses the sort order of the table.
@@ -27,3 +21,20 @@ TASK 2: Add a button to the HTML which reverses the sort order of the table.
 TASK 3: Write a function that validates the table by adding up all goalsPlus in the group and all goalsMinus in the group
 and outputs true if the difference is 0. Background the table green if it is valid or red if it is not.
 */
+
+const getReducerFunctionFor = (field) => {
+	return (totalGoals, currentRank) => {
+		return totalGoals + currentRank[field];
+	};
+};
+
+app.controller("tableController", ($http, $scope) => {
+	$http.get("https://sport.api.swisstxt.ch/v1/rankings/8481?lang=de").then(response => {
+		$scope.ranking = orderBy(response.data.rankingItems, "goalPlus", "desc");
+				
+		$scope.tableIsValid = () => {
+			return $scope.ranking.reduce(getReducerFunctionFor("goalPlus"), 0)
+			=== $scope.ranking.reduce(getReducerFunctionFor("goalMinus"), 0);
+		}
+	});
+});
